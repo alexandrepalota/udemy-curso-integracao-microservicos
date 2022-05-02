6e93fb1a652452e468a9c6bdf1326156c437a399
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-02 12:52:11 -0300

    Auth-API: criando repository e service

---
+++ auth-api/src/modules/user/service/UserService.js
@@ -0,0 +1,35 @@
+import UserRepository from './../repository/UserRepository'
+import * as httpStatus from './../../../config/constants/httpStatus'
+
+class UserService {
+
+    async findByEmail(req) {
+        try {
+            const {email} = req.params
+            this.validateRequestData(email)
+            let user = UserRepository.findByEmail(email)
+            if (!user) {
+
+            }
+            return {
+                status: httpStatus.SUCCESS,
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                }
+            }
+        } catch (error) {
+            return {
+                status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: error.status
+            }
+        }
+    }
+
+    validateRequestData(email) {
+        if (!email) throw new Error('User e-mail was not informed.')
+    }
+}
+
+export default new UserService()
