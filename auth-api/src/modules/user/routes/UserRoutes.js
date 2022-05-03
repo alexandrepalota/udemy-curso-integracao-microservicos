3120689e3d958bac3c2ecf184c8d719687f30fc1
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-02 22:08:44 -0300

    Auth-API: desenvolvendo token jwt

--- auth-api/src/modules/user/routes/UserRoutes.js
+++ auth-api/src/modules/user/routes/UserRoutes.js
@@ -5,5 +5,6 @@ import userController from "../controller/userController.js"
 const router = new Router()
 
 router.get('/api/users/email/:email', userController.findByEmail)
+router.post('/api/users/auth', userController.getAccessToken)
 
 export default router
--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,12 +1,15 @@
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import UserRepository from './../repository/UserRepository.js'
 import * as httpStatus from './../../../config/constants/httpStatus.js'
+import * as secrets from './../../../config/constants/secrets.js'
 import UserException from '../exceptions/UserException.js'
 
 class UserService {
 
     async findByEmail(req) {
         try {
-            const {email} = req.params
+            const { email } = req.params
             this.validateRequestData(email)
             let user = await UserRepository.findByEmail(email)
             this.validateUserNotFound(user)
@@ -23,6 +26,40 @@ class UserService {
                 status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
                 message: error.message
             }
+        }
+    }
+
+    async getAccessToken(req) {
+        try {
+            const { email, password } = req.body
+            this.validateAccessTokenData(email, password)
+            let user = await UserRepository.findByEmail(email)
+            this.validateUserNotFound(user)
+            await this.validatePassword(password, user.password)
+            const authUser = { id: user.id, name: user.name, email: user.email }
+            const accessToken = jwt.sign({ authUser }, secrets.apiSecret, { expiresIn: '1d'})
+            return  {
+                status: httpStatus.SUCCESS,
+                accessToken
+            }
+        } catch (error) {
+            return {
+                status: error.status ? error.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: error.message
+            }
+        }
+
+    }
+
+    async validatePassword(password, hashPassord) {
+        let isValid = await bcrypt.compare(password, hashPassord)
+        console.log(isValid)
+        if (!isValid) throw new UserException(httpStatus.UNAUTHORIZED, 'Password do not match')
+    }
+
+    validateAccessTokenData(email, password) {
+        if (!email || !password) {
+            throw new UserException(httpStatus.UNAUTHORIZED, 'E-mail and passord must be informed.')
         }
     }
 
