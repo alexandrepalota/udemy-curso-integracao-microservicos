5ff0065c8fabf9820977d44ac72f7fd9cf19071b
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-03 16:35:18 -0300

    Product-API: Criando entidades Product, Supplier e Category

---
+++ product-api/src/main/java/br/com/cursoudemy/productapi/model/Product.java
@@ -0,0 +1,38 @@
+package br.com.cursoudemy.productapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_CATEGORY", nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_SUPPLIER", nullable = false)
+    private Supplier supplier;
+
+}
